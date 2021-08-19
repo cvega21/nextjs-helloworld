52048691de9d2dab5b043760ced1f66eedc139c0
Christian Vega-Munguia <christianvega.cvm@gmail.com>
2021-08-18 22:46:18 -0500

    added api function and generic blog content

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,7 @@
 import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
+import Head from 'next/head'
+import utilStyles from '../../styles/utils.module.css'
 
 export async function getStaticProps({ params }) {
   const postData = await getPostData(params.id)
@@ -18,16 +20,19 @@ export async function getStaticPaths() {
   }
 }
 
-export default function Post ({ postData }) {
+export default function Post({ postData }) {
   return (
     <Layout>
-      {postData.title}
-      <br/>
-      {postData.id}
-      <br/>
-      {postData.date}
-      <div dangerouslySetInnerHTML={{__html: postData.contentHtml}}/>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   )
 }
-
